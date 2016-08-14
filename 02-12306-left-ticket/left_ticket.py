090ad07346dbc297104d0f420733773b01017240
梁文韬 <liangwentao@360.cn>
2016-08-14 20:26:47 +0800

    new feature

--- 02-12306-left-ticket/left_ticket.py
+++ 02-12306-left-ticket/left_ticket.py
@@ -4,33 +4,39 @@
 import argparse
 import os
 import json
-import urllib
 import urllib2
 import ssl
 import sys
 import re
+import socket
+from datetime import datetime
 
-PURPOSE_CODES = ['ADULT', '0X00'] # 成人票，学生票
-
+PURPOSE_CODES   = ['ADULT', '0X00'] # 成人票，学生票
+CITY_CACHE      = None
 CITY_CACHE_FILE = '.cities'
 CITY_LIST_URL   = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js'
 ACTION_URL      = 'https://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes={ticket_type}&queryDate={train_time}&from_station={from_city}&to_station={to_city}'
 
-def date_format(input_date):
-    def add_zero(month):
-        if int(month) < 10:
-            month = '0' + str(int(month))
-        return month
-    from datetime import datetime
+def add_zero(month):
+    if int(month) < 10:
+        month = '0' + str(int(month))
+    return month
+
+def default_date():
     now = datetime.now()
+    return '-'.join([str(now.year), str(add_zero(now.month)), str(now.day)])
+
+def date_format(input_date):
+    
     if not input_date:
-        return '-'.join([str(now.year), str(add_zero(now.month)), str(now.day)])
+        return default_date()
     res = re.match(r'(([0-9]{4})[-|\\|:])?([0-9]{1,2})[-|\\|:]([0-9]{2})', input_date)
     if res:
         year = res.group(2)
         month = res.group(3)
         day = res.group(4)
         
+        now = datetime.now()
         if not year:
             year = now.year
         if not month:
@@ -42,13 +48,13 @@ def date_format(input_date):
         print ('输入日期格式错误')
         sys.exit(-1)
 
-
-def _main(from_city, to_city, train_time, ticket_type):
+def load_cities():
+    global CITY_CACHE
+    if CITY_CACHE is not None:
+        return CITY_CACHE
     cache_file  = os.path.join(os.path.dirname(os.path.abspath(__file__)), CITY_CACHE_FILE)
-    ssl_ctx     = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-    cities      = {}
     need_relaod = False
-
+    cities      = {}
     if os.path.exists(cache_file):
         with open(cache_file, 'rb') as fp:
             cities = json.load(fp)
@@ -65,6 +71,13 @@ def _main(from_city, to_city, train_time, ticket_type):
             cities[city] = code
         with open(cache_file, 'w') as fp:
             json.dump(cities, fp)
+    CITY_CACHE = cities
+    return cities
+
+
+def search(from_city, to_city, train_time, ticket_type='ADULT'):
+    ssl_ctx     = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    cities      = load_cities()
 
     try:
         from_code = cities[from_city.decode('utf-8')]
@@ -99,17 +112,99 @@ def _main(from_city, to_city, train_time, ticket_type):
             r['ze_num']
         ))
 
+def getip():
+    url    = 'http://ip.chinaz.com/'
+    opener = urllib2.urlopen(url, timeout=5)
+    if url == opener.geturl():
+        info = opener.read()
+        res   = re.search('\d+\.\d+\.\d+\.\d+',info)
+        if res:
+            return res.group(0)
+    return None
+
+def getaddr(fresh=False):
+    addr_cache_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.addr')
+    if not fresh and os.path.exists(addr_cache_file):
+        addr = None
+        with open(addr_cache_file, 'rb') as fp:
+            addr = fp.read()
+        if addr:
+            return addr
+    ip   = getip()
+
+    if not ip:
+        return None
+    url  = 'http://ip.taobao.com/service/getIpInfo.php?ip=%s' % ip
+    info = urllib2.urlopen(url, timeout=5).read()
+    city = None
+    if info:
+        info = json.loads(info)
+        city = info['data']['city']
+        city = city.encode('utf-8').replace('市', '')
+        with open(addr_cache_file, 'w') as fp:
+            fp.write(city)
+    return city
+
+def get_yn_input(msg):
+    while True:
+        res = raw_input('%s，是请按回车，不是请输入n：' % msg)
+        if res in ('', 'n'):
+            break
+    return True if res == None else False
+
+def guide():
+    try:
+        cities = load_cities()
+        city   = getaddr()
+    except socket.timeout:
+        print ('请求超时')
+        sys.exit(-1)
+
+    if city and cities.has_key(city.decode('utf-8')):
+        from_city = raw_input('请输入起始城市（输入回车为%s）：' % city)
+        if not from_city:
+            from_city = city
+    else:
+        from_city = raw_input('请输入起始城市：')
+    while True:
+        to_city = raw_input('请输入目的城市：')
+        if to_city:
+            break
+
+    dd         = default_date()
+    train_time = raw_input('请输入出发日期（输入回车为%s）：' % dd)
+    if not train_time:
+        train_time = dd
+    else:
+        train_time = date_format(train_time)
+    ticket_type = 'ADULT' if get_yn_input('是否成人票') else '0X00'
+    print('正在查询...\n')
+    search(from_city, to_city, train_time, ticket_type)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='查询12306车次余票.')
     parser.add_argument('-f', '--from_city', type=str, help='起始城市')
     parser.add_argument('-t', '--to_city', type=str, help='目标城市')
     parser.add_argument('-d', '--date', type=str, help='日期，格式如：2016-08-14')
     parser.add_argument('-s', '--student', action='store_true', help='学生票')
+    parser.add_argument('-l', '--list_city', action='store_true', help='查看支持城市列表')
 
     args        = parser.parse_args()
     from_city   = args.from_city
     to_city     = args.to_city
     train_time  = date_format(args.date)
     ticket_type = PURPOSE_CODES[1] if args.student is True else PURPOSE_CODES[0]
-    _main(from_city, to_city, train_time, ticket_type)
+    list_city   = args.list_city
+    
+    if from_city is None and to_city is None and list_city is False:
+        guide()
+    else:
+        if list_city:
+            for city, code in load_cities().items():
+                print city,
+        elif from_city and to_city and ticket_type:
+            search(from_city, to_city, train_time, ticket_type)
+        else:
+            print ('参数错误')
 
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # one-week
-一周一个小项目，纯粹个人娱乐：）
+一周一个小项目，纯粹个人娱乐:)
 
  - 01. [file-classifier(目录文件分类工具)](https://github.com/tonnie17/one-week/tree/master/01-file-classifier)
+ - 02. [12306-left-ticket(12306余票查询工具)](https://github.com/tonnie17/one-week/tree/master/02-12306-left-ticket)
