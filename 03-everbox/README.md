a793692a95cd64b3985c9097f4e5850c0e4daec5
梁文韬 <liangwentao@360.cn>
2016-08-16 18:59:40 +0800

    修改说明

--- 03-everbox/README.md
+++ 03-everbox/README.md
@@ -1,3 +1,155 @@
 # everbox.py
 
 evernote as a file sandbox!
+
+## 用法
+
+```
+usage: everbox.py [-h] {push,pushall,list,pull,log} ...
+
+文本备份助手.
+
+optional arguments:
+  -h, --help            show this help message and exit
+
+操作命令:
+  {push,pushall,list,pull,log}
+    push                添加文本到仓库
+    pushall             添加批量文本到仓库
+    list                列出仓库文本
+    pull                从仓库拉取文本
+    log                 查看文本记录信息
+```
+
+## 准备工作
+
+安装evernote sdk for python
+
+```
+pip install evernote
+```
+
+登录[Evernote开发者](http://sandbox.evernote.com)，获取开发Token，把获取到的token替换掉代码中的dev_token。
+
+## 基本操作
+
+### list 列出仓库或文本
+
+```
+usage: everbox.py list [-h] [box]
+
+列出仓库文本
+
+positional arguments:
+  box         仓库id或仓库名字
+```
+
+获取所有仓库
+
+```
+python everbox.py list
+```
+
+```
+| 文本id          | 仓库名称 |
+6da27e72-ad2d-4cd0-a05a-f1fc12d9e44c 我的第一个笔记本
+1902a691-62f3-4edc-a8bb-4db6d949da50 示例笔记本
+```
+
+获取仓库文本
+
+```
+python everbox.py list 6da2
+```
+
+```
+| 文本id          | 文本名称 |
+b00204f8-41d0-43bb-8fc3-17b3a654360f README.md
+f7c7b2be-c247-4c2a-8001-186d27942cce README.md
+```
+
+
+### pushall 推送所有文本
+
+```
+usage: everbox.py pushall [-h] [-b BOX] [files [files ...]]
+
+添加批量文本到仓库
+
+positional arguments:
+  files              文本路径，多个以空格间隔
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -b BOX, --box BOX  仓库id或仓库名字
+```
+
+```
+python everbox.py pushall -b 6da2 README.md
+```
+
+```
+已上传(1/1)个文本
+```
+
+```
+python everbox.py pushall README.md
+```
+
+```
+无指定仓库，将使用默认仓库
+已上传(1/1)个文本
+```
+
+
+### log 查看文件在仓库中的记录
+
+```
+usage: everbox.py log [-h] file
+
+查看文本记录信息
+
+positional arguments:
+  file        文本名称
+```
+
+```
+python everbox.py log README.md
+```
+
+输出
+
+```
+| 文本id          | 文本名称 | 仓库  | 创建时间
+b00204f8-41d0-43bb-8fc3-17b3a654360f README.md 我的第一个笔记本 2016-08-16 17:14:07
+f7c7b2be-c247-4c2a-8001-186d27942cce README.md 我的第一个笔记本 2016-08-16 17:15:02
+```
+
+## pull 从仓库中拉取文件
+
+```
+usage: everbox.py pull [-h] [-b BOX] [-y] [files [files ...]] directory
+
+从仓库拉取文本
+
+positional arguments:
+  files              文本guid或名称（若用名称则取最新的同名
+                     )，多个以空格间隔
+  directory          拉取目录
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -b BOX, --box BOX  仓库id或仓库名字
+  -y, --yes          忽略覆盖提示
+```
+
+```
+python everbox.py pull b00204f8-41d0-43bb-8fc3-17b3a654360f  .
+```
+
+输出
+
+```
+文件 /Users/tonnie/github/one-week/03-everbox/README.md 已存在，是否覆盖，是请按y，不是请输入n：y
+成功拉取：1个文件
+```
--- 03-everbox/everbox.py
+++ 03-everbox/everbox.py
@@ -5,7 +5,7 @@ import binascii
 import hashlib
 import os
 import sys
-import md5
+from datetime import datetime, date
 
 try:
     import evernote
@@ -38,9 +38,9 @@ class LazyGet:
         print (self._end_hint)
         return self._ins
 
-get_client    = LazyGet(lambda : EvernoteClient(token=dev_token), '连接evernote...', '连接成功', '连接失败')
-get_box_store = LazyGet(lambda : get_client().get_note_store(), '获取数据库...', '获取成功', '获取失败')
-get_boxes     = LazyGet(lambda : get_box_store().listNotebooks(), '获取仓库信息...', '获取成功', '获取失败')
+get_client    = LazyGet(lambda : EvernoteClient(token=dev_token), '初始化组件...', '初始化成功', '初始化失败')
+get_box_store = LazyGet(lambda : get_client().get_note_store(), '连接evernote...', '连接成功', '连接失败')
+get_boxes     = LazyGet(lambda : get_box_store().listNotebooks(), '获取仓库信息...', '获取仓库成功', '获取失败')
 
 def create_box(name):
     box      = Types.Notebook()
@@ -51,12 +51,12 @@ def create_file(title, content, box_id='', image_resource=None):
     hash_hex = ''
     file = Types.Note()
     if box_id:
-        file.box_id = box_id
+        file.notebookGuid = box_id
 
     if image_resource:
         (resource, image_hash) = image_resource
-        file.resources = [resource]
-        hash_hex = binascii.hexlify(image_hash)
+        file.resources         = [resource]
+        hash_hex               = binascii.hexlify(image_hash)
 
     file.title = title
     file.content = '''<?xml version="1.0" encoding="UTF-8"?>
@@ -68,37 +68,40 @@ def create_file(title, content, box_id='', image_resource=None):
 
     return file
 
-# def create_image_resource(image):
-#     md5 = hashlib.md5()
-#     md5.update(image)
-#     image_hash = md5.digest()
+def parse_file(content):
+    file_content = ''
+    try:
+        content = content.split('<caption>')[1].split('</caption>')[0]
+    except:
+        pass
 
-#     data = Types.Data()
-#     data.size = len(image)
-#     data.bodyHash = image_hash
-#     data.body = image
+    media_index = content.find('<en-media')
+    content = content[:media_index]
 
-#     resource = Types.Resource()
-#     resource.mime = 'image/png'
-#     resource.data = data
+    file_lst = content.split('<br/>')
+    for content in file_lst:
+        file_content += content
 
-#     return (resource, image_hash)
+    return file_content
 
-# def parse_note(content):
-#     note_content = ''
-#     try:
-#         content = content.split('<caption>')[1].split('</caption>')[0]
-#     except:
-#         pass
+def load_file(guid):
+    file = None
+    try:
+        file = get_box_store().getNote(dev_token,
+                                  guid,
+                                  True,
+                                  True,
+                                  False,
+                                  False)
+    except Exception, e:
+        return None
 
-#     media_index = content.find('<en-media')
-#     content = content[:media_index]
-
-#     note_lst = content.split('<br/>')
-#     for content in note_lst:
-#         note_content += content
-
-#     return note_content
+    file_content = parse_file(file.content)
+    return {
+        'title': file.title,
+        'content': file_content,
+        'created': date.fromtimestamp(file.created / 100).strftime('%d/%m/%Y')
+    }
 
 def get_box_by_id(box_id):
     if len(box_id) < 4:
@@ -116,6 +119,8 @@ def get_box_by_name(name):
     return ''
 
 def get_box(id_or_name):
+    if not id_or_name:
+        return ''
     box = get_box_by_id(args.box)
     if not box:
         box = get_box_by_name(args.box)
@@ -127,24 +132,67 @@ def get_box(id_or_name):
 def push_to_box(note):
     get_box_store().createNote(dev_token, note)
 
+def get_yn_input(msg):
+    while True:
+        res = raw_input('%s，是请按y，不是请输入n：' % msg)
+        if res in ('y', 'n'):
+            break
+    return True if res == 'y' else False
+
 def pull(args):
-    print args
+    dest_dir = args.directory or os.path.expanduser('~')
+    if not os.path.exists(dest_dir) or not os.path.isdir(dest_dir):
+        print ('指定目录不存在')
+        return
+    dest_dir   = os.path.abspath(os.path.normpath(dest_dir))
+    skip_cover = args.yes
+    finished   = 0
+    for f in args.files:
+        try:
+            flag       = os.path.basename(f)
+            file       = load_file(flag)
+            if not file:
+                box         = get_box(args.box)
+                file_filter = NoteTypes.NoteFilter()
+                if box:
+                    file_filter.notebookGuid = box.guid
+                file_filter.title = flag
+                files             = get_box_store().findNotes(dev_token, file_filter, 0, 100).notes
+                file              = load_file(files[-1].guid)
+            if not file:
+                raise
+            ouput_file = os.path.join(dest_dir, file['title'])
+            if os.path.exists(ouput_file):
+                if not skip_cover:
+                    if not get_yn_input('文件 %s 已存在，是否覆盖' % ouput_file):
+                        continue
+            with open(ouput_file, 'w') as fp:
+                fp.write(file['content'])
+            finished += 1
+            sys.stdout.write('已拉取：%s个文件\r' % finished)
+            sys.stdout.flush()
+        except Exception,e:
+            print e
+            print ('文件：%s拉取失败，跳过..' % f)
+    print('成功拉取：%s个文件' % finished)
 
 def list(args):
-    print ('查询中...\n')
-
     def list_box(box):
-        note_filter              = NoteTypes.NoteFilter()
-        note_filter.notebookGuid = box.guid
-        notes                    = get_box_store().findNotes(dev_token, note_filter, 0, 100).notes
+        file_filter              = NoteTypes.NoteFilter()
+        file_filter.notebookGuid = box.guid
+        files                    = get_box_store().findNotes(dev_token, file_filter, 0, 100).notes
 
-        print ('| 文件id\t\t\t| 文件名称 |')
-        for note in notes:
-            print "%s %s" %(note.guid, note.title)
+        if not files:
+            print '仓库没有任何文本'
+        else:
+            print ('| 文本id\t\t\t| 文本名称 |')
+            for f in files:
+                print "%s %s" %(f.guid, f.title)
 
     if args.box is None:
-        print ('| 文件id\t\t\t| 仓库名称 |')
-        for box in get_boxes():
+        boxes = get_boxes()
+        print ('| 文本id\t\t\t| 仓库名称 |')
+        for box in boxes:
             print box.guid, box.name
             # n.serviceCreated, n.serviceUpdated
     else:
@@ -156,12 +204,15 @@ def push(args):
     file = os.path.exists(os.path.abspath(os.path.normpath(args.file)))
 
 def pushall(args):
-    box    = get_box(args.box)
     files  = args.files
     to_add = []
     for f in files:
-        if not os.path.exists(os.path.abspath(os.path.normpath(f))):
-            print ('%s：文件路径不存在' %f)
+        abs_path = os.path.abspath(os.path.normpath(f))
+        if not os.path.exists(abs_path):
+            print ('%s：文本路径不存在' %f)
+            break
+        if os.path.getsize(abs_path) > 1000 * 1000 * 10: # 10M
+            print ('%s: 文本体积大于10M' %f)
             break
         to_add.append(os.path.abspath(os.path.normpath(f)))
     else:
@@ -170,47 +221,70 @@ def pushall(args):
         for f in to_add:
             try:
                 with open(f, 'rb') as fp:
-                    title             = md5.md5(f).hexdigest()
-                    file              = create_file(title, fp.read(), box.guid, '')
-                    file.notebookGuid = box.guid
+                    title             = os.path.basename(f)
+                    if args.box:
+                        box  = get_box(args.box)
+                        file = create_file(title, fp.read(), box.guid, '')
+                    else:
+                        print ('无指定仓库，将使用默认仓库')
+                        file = create_file(title, fp.read(), None, '')
                     push_to_box(file)
                     finished += 1
-                    sys.stdout.write('已上传(%s/%s)个文件\r' % (finished, total))
+                    sys.stdout.write('已上传(%s/%s)个文本\r' % (finished, total))
                     sys.stdout.flush()
             except Exception, e:
                 print (e)
-                print ('发生异常，程序退出!')
-        print('已上传(%s/%s)个文件' % (finished, total))
+                print ('上传%s时，发生异常' % f)
+        print('已上传(%s/%s)个文本' % (finished, total))
 
 
 def log(args):
-    print args
+    file     = args.file
+    abs_path = os.path.abspath(os.path.normpath(file))
+    if not os.path.exists(abs_path):
+        print ('文件不存在!')
+        return
+    file_name         = os.path.basename(abs_path)
+    file_filter       = NoteTypes.NoteFilter()
+    file_filter.title = file_name
+    files             = get_box_store().findNotes(dev_token, file_filter, 0, 100).notes
+    if not files:
+        print ('仓库中不存在该文件的记录')
+        return
+    print ('| 文本id\t\t\t| 文本名称 | 仓库\t | 创建时间')
+    for f in files:
+        print '%s %s %s %s' %(f.guid, f.title, get_box_by_id(f.notebookGuid).name, 
+            datetime.fromtimestamp(int(str(f.created)[:-3])).strftime('%Y-%m-%d %H:%M:%S'))
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='文本备份助手.')
     subparsers = parser.add_subparsers(title='操作命令')
 
-    push_cmd  = subparsers.add_parser('push', help='添加文件到仓库', description='添加文件到仓库')
-    push_cmd.add_argument('box', help='仓库id或仓库名字')
+    push_cmd  = subparsers.add_parser('push', help='添加文本到仓库', description='添加文本到仓库')
+    push_cmd.add_argument('-b', '--box', help='仓库id或仓库名字')
     push_cmd.add_argument('file')
     push_cmd.set_defaults(func=push)
 
-    push_all_cmd  = subparsers.add_parser('pushall', help='添加批量文件到仓库', description='添加批量文件到仓库')
-    push_all_cmd.add_argument('box', help='仓库id或仓库名字')
-    push_all_cmd.add_argument('files', nargs='*')
+    push_all_cmd  = subparsers.add_parser('pushall', help='添加批量文本到仓库', description='添加批量文本到仓库')
+    push_all_cmd.add_argument('-b', '--box', help='仓库id或仓库名字')
+    push_all_cmd.add_argument('files', nargs='*', help='文本路径，多个以空格间隔')
     push_all_cmd.set_defaults(func=pushall)
 
-    list_cmd = subparsers.add_parser('list', help='列出仓库文件', description='列出仓库文件')
-    list_cmd.add_argument('box', nargs='?')
+    list_cmd = subparsers.add_parser('list', help='列出仓库文本', description='列出仓库文本')
+    list_cmd.add_argument('box', nargs='?', help='仓库id或仓库名字')
     list_cmd.set_defaults(func=list)
 
-    pull_cmd = subparsers.add_parser('pull', help='从仓库拉取文件', description='从仓库拉取文件')
-    pull_cmd.add_argument('tag')
+    pull_cmd = subparsers.add_parser('pull', help='从仓库拉取文本', description='从仓库拉取文本')
+    pull_cmd.add_argument('-b', '--box', help='仓库id或仓库名字')
+    pull_cmd.add_argument('-y', '--yes', action='store_true', help='忽略覆盖提示')
+    pull_cmd.add_argument('files', nargs='*', help='文本guid或名称（若用名称则取最新的同名），多个以空格间隔')
+    pull_cmd.add_argument('directory', type=str, help='拉取目录')
     pull_cmd.set_defaults(func=pull)
 
-    log_cmd = subparsers.add_parser('log', help='查看文件记录信息', description='查看文件记录信息')
-    log_cmd.add_argument('file')
+    log_cmd = subparsers.add_parser('log', help='查看文本记录信息', description='查看文本记录信息')
+    log_cmd.add_argument('file', help='文本名称')
     log_cmd.set_defaults(func=log)
 
     args = parser.parse_args()
