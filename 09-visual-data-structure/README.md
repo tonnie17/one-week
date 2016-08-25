817a60812606cc6f9b5c41178b88cc541a6ab07f
梁文韬 <liangwentao@360.cn>
2016-08-25 13:00:36 +0800

    修改实现

--- 09-visual-data-structure/README.md
+++ 09-visual-data-structure/README.md
@@ -2,15 +2,23 @@
 
 使用PIL的可视化数据结构。
 
+## 数据结构类需要实现的接口
+
+```
+def __iter__(self) => obj # 遍历容器元素的方法
+def get_size(self) => int # 获取容器元素数量的方法
+```
+
 ## 栈
 
 ```
-from vds import Stack
+from vds import StackVds
 
 s = Stack([1,24,23,423])
-s.show()
+tracer = StackVds(s)
+tracer.show()
 s.pop()
-s.show()
+tracer.show()
 ```
 
 ![Stack](http://ww3.sinaimg.cn/large/006tNc79jw1f75ubfky98j31kw0eedh1.jpg)
@@ -18,12 +26,13 @@ s.show()
 ## 链表
 
 ```
-from vds import LinkList
+from vds import LinkListVds
 
 l = LinkList(1, [2,3,4,3])
-l.show()
+tracer = LinkListVds(l)
+tracer.show()
 l.delete(3, all=True)
-l.show()
+tracer.show()
 ```
 
 ![Linklist](http://ww3.sinaimg.cn/large/006tNc79jw1f75ubfxl1cj31kw0ea75p.jpg)
--- 09-visual-data-structure/vds.py
+++ 09-visual-data-structure/vds.py
@@ -1,20 +1,21 @@
 #-*- coding:utf-8 -*-
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
-class List(object):
+class Vds(object):
+    def __init__(self, obj):
+        self.object = obj
+
+    def show():
+        raise NotImplementedError
+
+class ListVds(Vds):
     MAX_SIZE   = 20
     PER_HEIGHT = 50
     WIDTH      = 300
 
-    def __init__(self, array=None):
-        self.array = array if isinstance(array, list) else []
-
-    def get_size(self):
-        return len(self.array)
-
     def show(self):
         width  = self.WIDTH
-        height = self.get_size() * self.PER_HEIGHT + 3
+        height = self.object.get_size() * self.PER_HEIGHT + 3
         image  = Image.new('RGB', (width, height + 6), (255, 255, 255))
         font   = ImageFont.truetype('Arial.ttf', 36)
         draw   = ImageDraw.Draw(image)
@@ -22,98 +23,26 @@ class List(object):
         start  = width / 3
         end    = width * 2 / 3
 
-        for i, ele in enumerate(self.array):
+        for i, ele in enumerate(self.object):
             draw.rectangle([start, height - (i + 1) * offset, end, height - i * offset], outline=(0,0,0))
             draw.text([width/2, height - (i + 1) * offset + offset/2], str(ele), fill=(0,0,0))
 
         image.show()
 
-    def __getattr__(self, key):
-        if hasattr(self.array, key):
-            return getattr(self.array, key)
-        return super().__getattr__(key)
+class StackVds(ListVds):
+    pass
 
-
-class Stack(List):
-    def push(self, val):
-        if self.get_size() == self.MAX_SIZE:
-            print ('超过最大容量')
-        else:
-            self.array.append(val)
-
-    def pop(self):
-        return self.array.pop()
-
-    def head(self):
-        if self.array:
-            return self.array[-1]
-        return None
-
-    def clear(self):
-        self.array.clear()
-
-    
-class LinkList(object):
+class LinkListVds(Vds):
     MAX_SIZE   = 20
     PER_WIDTH  = 100
     HEIGHT     = 200
 
-    def __init__(self, val, nodes=None):
-        self.next = None
-        self.val  = val
-        if isinstance(nodes, list) and nodes:
-            self.mul_add(nodes)
-
-    def add(self, val):
-        node = self
-        while node.next is not None:
-            node = node.next
-        child     = LinkList(val)
-        node.next = child
-        self.childs += 1
-
-    def delete(self, val, all=False):
-        node = prev = self
-        while node and node.val != val:
-            prev = node
-            node = node.next
-        if node:
-            prev.next = node.next
-            node = None
-            node = prev.next
-            if all == True:
-                node.delete(val)
-
-    def mul_add(self, datalist):
-        old = self
-        for val in datalist:
-            node = LinkList(val)
-            old.next = node
-            old = node
-
-    def traverse(self):
-        node = self
-        while node:
-            yield node
-            node = node.next
-
-    def get_size(self):
-        size = 1
-        node = self
-        while node.next is not None:
-            node = node.next
-            size += 1
-        return size
-
-    def __str__(self):
-        return str(self.val)
-
     def show(self):
-        size = self.get_size()
+        size = self.object.get_size()
         if size > self.MAX_SIZE:
             print ('超过最大容量')
             return
-        width   = self.get_size() * self.PER_WIDTH + 3
+        width   = size * self.PER_WIDTH + 3
         height  = self.HEIGHT
 
         image  = Image.new('RGB', (width + 6, height), (255, 255, 255))
@@ -124,8 +53,7 @@ class LinkList(object):
         end    = height * 2 / 3
         dis    = 10
 
-        i = 0
-        for node in self.traverse():
+        for i, node in enumerate(self.object):
             draw.rectangle([width - (i + 1) * offset, start, width - i * offset - dis, end], outline=(0,0,0))
             draw.text([i * offset + offset/2, height/2], str(node), fill=(0,0,0))
             draw.line((width - i * offset - dis, (start+end)/2, width - i * offset, (start+end)/2), fill=(0,0,0))
@@ -133,15 +61,112 @@ class LinkList(object):
 
         image.show()
 
+
+
+
+
 def _main():
+    class List(object):
+        MAX_SIZE   = 20
+        PER_HEIGHT = 50
+        WIDTH      = 300
+
+        def __init__(self, array=None):
+            self.array = array if isinstance(array, list) else []
+
+        def get_size(self):
+            return len(self.array)
+
+        def __iter__(self):
+            return iter(self.array)
+
+        def __getattr__(self, key):
+            if hasattr(self.array, key):
+                return getattr(self.array, key)
+            return super().__getattr__(key)
+            
+    class Stack(List):
+        def push(self, val):
+            if self.get_size() == self.MAX_SIZE:
+                print ('超过最大容量')
+            else:
+                self.array.append(val)
+
+        def pop(self):
+            return self.array.pop()
+
+        def head(self):
+            if self.array:
+                return self.array[-1]
+            return None
+
+        def clear(self):
+            self.array.clear()
+
+        
+    class LinkList(object):
+
+        def __init__(self, val, nodes=None):
+            self.next = None
+            self.val  = val
+            if isinstance(nodes, list) and nodes:
+                self.mul_add(nodes)
+
+        def add(self, val):
+            node = self
+            while node.next is not None:
+                node = node.next
+            child     = LinkList(val)
+            node.next = child
+            self.childs += 1
+
+        def delete(self, val, all=False):
+            node = prev = self
+            while node and node.val != val:
+                prev = node
+                node = node.next
+            if node:
+                prev.next = node.next
+                node = None
+                node = prev.next
+                if all == True:
+                    node.delete(val)
+
+        def mul_add(self, datalist):
+            old = self
+            for val in datalist:
+                node = LinkList(val)
+                old.next = node
+                old = node
+
+        def __iter__(self):
+            node = self
+            while node:
+                yield node
+                node = node.next
+
+        def get_size(self):
+            size = 1
+            node = self
+            while node.next is not None:
+                node = node.next
+                size += 1
+            return size
+
+        def __str__(self):
+            return str(self.val)
+
     s = Stack([1,24,23,423])
-    s.show()
+    tracer = StackVds(s)
+    tracer.show()
     s.pop()
-    s.show()
+    tracer.show()
+
     l = LinkList(1, [2,3,4,3])
-    l.show()
+    tracer = LinkListVds(l)
+    tracer.show()
     l.delete(3, all=True)
-    l.show()
+    tracer.show()
 
 
 if __name__ == '__main__':
