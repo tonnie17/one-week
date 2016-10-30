fdac828b0115db77bfb249b6fa1c65fc4019caf6
梁文韬 <liangwentao@360.cn>
2016-10-30 17:20:30 +0800

    update

--- 13-async-demo/README.md
+++ 13-async-demo/README.md
@@ -1,3 +1,27 @@
-# data_gen.php
+# async_demo.py
 
-async make your life more beautiful.
+基于异步io的爬虫演示。
+
+# 使用
+
+```
+from async_demo import AsyncWorker, AsyncRequest
+
+def fetch(url):
+    request = AsyncRequest('www.baidu.com', url, 80)
+    data = yield from request.process()
+    return data
+
+def get_page(url):
+    page = yield from fetch(url)
+    return page
+
+def print_content_length(data):
+    print(len(data))
+
+async_worker = AsyncWorker(get_page, workers=20, loop_timeout=5)
+async_worker.add_result_callback(print_content_length)
+for i in range(15):
+    async_worker.put('/s?wd={}'.format(i))
+async_worker.work()
+```
--- 13-async-demo/async_demo.py
+++ 13-async-demo/async_demo.py
@@ -4,18 +4,16 @@ from selectors import DefaultSelector, EVENT_WRITE, EVENT_READ
 selector = DefaultSelector()
 
 class StopError(BaseException):
-    """Raised to stop the event loop."""
     pass
 
-def stop_callback(future):
-    raise StopError
-
+class SelectTimeout(BaseException):
+    pass
 
 class EventLoop:
     stopped = False
+    select_timeout = 5
 
     def run_until_complete(self, coros):
-        self.unfinished_tasks = len(coros)
         tasks = [Task(coro) for coro in coros]
         try:
             self.run_forever()
@@ -24,16 +22,15 @@ class EventLoop:
 
     def run_forever(self):
         while not self.stopped:
-            events = selector.select()
+            events = selector.select(self.select_timeout)
+            if not events:
+                raise SelectTimeout('轮询超时')
             for event_key, event_mask in events:
                 callback = event_key.data
                 callback(event_key, event_mask)
 
-    def task_finish(self):
-        self.unfinished_tasks -= 1
-        if self.unfinished_tasks == 0:
-            self.stopped = True
-
+    def close(self):
+        self.stopped = True
 
 __ev_loop = EventLoop()
 
@@ -64,8 +61,9 @@ class Task(Future):
     def step(self, future):
         try:
             next_future = self.coro.send(future.result)
+            if next_future is None:
+                return
         except StopIteration as exc:
-            get_event_loop().task_finish()
             self.set_result(exc.value)
             return
         next_future.add_done_callback(self.step)
@@ -77,7 +75,7 @@ def read(sock):
         f.set_result(sock.recv(4096))
 
     selector.register(sock.fileno(), EVENT_READ, on_readable)
-    chunk = yield f  # Read one chunk.
+    chunk = yield f
     selector.unregister(sock.fileno())
     return chunk
 
@@ -101,19 +99,14 @@ class AsyncRequest:
         self.port = port
         self.method = None
 
-    def process(self):
-        chunk = yield from self.gen()
-        return chunk
-
     def get(self):
         self.method = 'GET'
         self.request = '{} {} HTTP/1.0\r\nHost: {}\r\n\r\n'.format(self.method, self.url, self.host)
         return self
 
-    def gen(self):
+    def process(self):
         if self.method is None:
             self.get()
-        self.connected = False
         try:
             self.sock.connect((self.host, self.port))
         except BlockingIOError:
@@ -124,7 +117,6 @@ class AsyncRequest:
                       self.on_connected)
         yield self.f
         selector.unregister(self.sock.fileno())
-        print('connected')
 
         self.sock.send(self.request.encode('ascii'))
 
@@ -132,7 +124,6 @@ class AsyncRequest:
         return chunk
 
     def on_connected(self, key, mask):
-        self.connected = True
         self.f.set_result(None)
 
 def fetch(url):
@@ -142,33 +133,96 @@ def fetch(url):
 
 def get_page(url):
     page = yield from fetch(url)
-    print('done!!')
+    return page
 
-def async_way():
-    ev_loop = get_event_loop()
-    ev_loop.run_until_complete([
-        get_page('/s?wd={}'.format(i)) for i in range(100)
-    ])    
+# def async_way():
+#     ev_loop = get_event_loop()
+#     ev_loop.run_until_complete([
+#         get_page('/s?wd={}'.format(i)) for i in range(100)
+#     ])
 
-def sync_way():
-    for i in range(100):
-        sock = socket.socket()
-        sock.connect(('www.baidu.com', 80))
-        print('connected')
-        request = 'GET {} HTTP/1.0\r\nHost: www.baidu.com\r\n\r\n'.format('/s?wd={}'.format(i))
-        sock.send(request.encode('ascii'))
-        response = b''
-        chunk = sock.recv(4096)
-        while chunk:
-            response += chunk
-            chunk = sock.recv(4096)
-        print('done!!')
+# def sync_way():
+#     for i in range(100):
+#         sock = socket.socket()
+#         sock.connect(('www.baidu.com', 80))
+#         print('connected')
+#         request = 'GET {} HTTP/1.0\r\nHost: www.baidu.com\r\n\r\n'.format('/s?wd={}'.format(i))
+#         sock.send(request.encode('ascii'))
+#         response = b''
+#         chunk = sock.recv(4096)
+#         while chunk:
+#             response += chunk
+#             chunk = sock.recv(4096)
+#         print('done!!')
 
-from time import time
+# from time import time
 # start = time()
 
-async_way() # Cost 3.534296989440918 seconds
-sync_way()  #Cost 47.757508993148804 seconds
+# async_way() # Cost 3.534296989440918 seconds
+# sync_way()  #Cost 47.757508993148804 seconds
 
 # end = time()
 # print ('Cost {} seconds'.format(end - start))
+
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self._q = deque()
+        self.size = 0
+
+    def put(self, item):
+        self.size += 1
+        self._q.append(item)
+
+    def get(self):
+        item = self._q.popleft()
+        return item
+
+    def task_done(self):
+        self.size -= 1
+        if self.size == 0:
+            self.empty_callback()
+
+class AsyncWorker(Queue):
+    def __init__(self, coroutine, workers=10, loop_timeout=5):
+        super().__init__()
+        self.func = coroutine
+        self.stopped = False
+        self.ev_loop = get_event_loop()
+        self.ev_loop.select_timeout = loop_timeout
+        self.workers = workers
+        self.result_callbacks = []
+
+    def work(self):
+        def _work():
+            while not self.stopped:
+                item = None
+                try:
+                    item = self.get()
+                except IndexError:
+                    yield None
+                    self.stopped = True
+                result = yield from self.func(item)
+                self.task_done()
+                for rcb in self.result_callbacks:
+                    rcb(result)
+        self.tasks = []
+        for _ in range(self.workers):
+            self.tasks.append(_work())
+        self.ev_loop.run_until_complete(self.tasks)
+
+    def add_result_callback(self, func):
+        self.result_callbacks.append(func)
+
+    def empty_callback(self):
+        self.ev_loop.close()
+
+def print_content_length(data):
+    print(len(data))
+
+async_worker = AsyncWorker(get_page, workers=20)
+async_worker.add_result_callback(print_content_length)
+for i in range(15):
+    async_worker.put('/s?wd={}'.format(i))
+async_worker.work()
